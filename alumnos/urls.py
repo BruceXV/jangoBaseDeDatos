a71c5bf998130dddbf7aab352931b8a291e8ab4a
unknown <SSDD@PNAWSA06LC1025.duoc.acad>
2024-06-10 16:32:12 -0400

    segundo commit

--- alumnos/urls.py
+++ alumnos/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [    
-    path('index', views.index , name="index")
+    path('index', views.index , name="index"),
+    path('crud', views.crud, name="crud"),
+    path('almunosAdd', views.alumnosAdd, name="alumnosAdd"),
+    path('almunos_del/<str:pk>', views.alumnos_del, name="alumnos_del"),
+    path('almunos_findEdit/<str:pk>', views.almunos_findEdit, name="alumnos_findEdit")
+    
 ]
